dc5b140c61127429e29f6c096f03a2e1f86ad7cd
pkulas-nlb <ext.pascal.kulas@nordlb.de>
2021-08-26 15:11:26 +0200

    Start Request with variables - ExceptionTask now only throws exception when requested

--- src/main/java/de/neusta/workflow/ExceptionTask.java
+++ src/main/java/de/neusta/workflow/ExceptionTask.java
@@ -1,30 +1,18 @@
 package de.neusta.workflow;
 
-import org.camunda.bpm.engine.RuntimeService;
+import lombok.extern.slf4j.Slf4j;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 
-import javax.inject.Inject;
-import java.util.Random;
-
+@Slf4j
 public class ExceptionTask extends LogginTask implements JavaDelegate {
 
-    private final Logger log = LoggerFactory.getLogger(ExceptionTask.class.getCanonicalName());
+    @Override
+    public void execute(DelegateExecution delegateExecution) {
+        log(delegateExecution, log);
 
-        @Autowired
-        RuntimeService runtimeService;
-
-        @Override
-    public void execute(DelegateExecution delegateExecution) throws Exception {
-
-
-        log(delegateExecution,log);
-
-            if( new Random().nextBoolean())
-        throw new RuntimeException();
-
+        if ((Boolean) delegateExecution.getVariable("throwexception")) {
+            throw new RuntimeException();
+        }
     }
 }
